f84b2c55711266ae61378d8bda25d91a91c91eae
Vo Trung Tin <tinvo2005@gmail.com>
2025-10-01 23:00:03 +0700

    commented out unit test

--- src/main/java/com/example/bookverseserver/mapper/UserProfileMapper.java
+++ src/main/java/com/example/bookverseserver/mapper/UserProfileMapper.java
@@ -11,19 +11,19 @@ public interface UserProfileMapper {
 
     UserProfile toEntity(ProfileCreationRequest request);
 
-//    @Mapping(source = "user.id", target = "userId")
-//    @Mapping(source = "displayName", target = "displayName")
-//    @Mapping(source = "phoneNumber", target = "phoneNumber")
-//    @Mapping(source = "avatarUrl", target = "avatarUrl", ignore = true)
-//    @Mapping(source = "bio", target = "bio", ignore = true)
-//    @Mapping(source = "accountType", target = "accountType", ignore = true)
-//    @Mapping(source = "location", target = "location")
-//    @Mapping(source = "ratingAvg", target = "ratingAvg")
-//    @Mapping(source = "ratingCount", target = "ratingCount")
-//    @Mapping(source = "sellerSince", target = "sellerSince", ignore = true)
-//    @Mapping(source = "preferences", target = "preferences")
-//    @Mapping(source = "createdAt", target = "createdAt")
-//    @Mapping(source = "updatedAt", target = "updatedAt")
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "displayName", target = "displayName")
+    @Mapping(source = "phoneNumber", target = "phoneNumber")
+    @Mapping(source = "avatarUrl", target = "avatarUrl", ignore = true)
+    @Mapping(source = "bio", target = "bio", ignore = true)
+    @Mapping(source = "accountType", target = "accountType", ignore = true)
+    @Mapping(source = "location", target = "location")
+    @Mapping(source = "ratingAvg", target = "ratingAvg")
+    @Mapping(source = "ratingCount", target = "ratingCount")
+    @Mapping(source = "sellerSince", target = "sellerSince", ignore = true)
+    @Mapping(source = "preferences", target = "preferences")
+    @Mapping(source = "createdAt", target = "createdAt")
+    @Mapping(source = "updatedAt", target = "updatedAt")
     ProfileResponse toResponse(UserProfile profile);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
