a0932fe7c84af4ab3f0d3bf688d503d0ed4b1b53
Vo Trung Tin <tinvo2005@gmail.com>
2025-10-15 02:41:01 +0700

    migrate to supabase cloud db

--- src/main/java/com/example/bookverseserver/entity/User/User.java
+++ src/main/java/com/example/bookverseserver/entity/User/User.java
@@ -67,6 +67,10 @@ public class User {
     String authProvider;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "role_id")
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
     Role role;
 }
