47e9970ca92147d698cabea9b460426edb7ba552
MKwang <tmq1512005@gmail.com>
2025-11-09 17:41:40 +0700

    feat: add mapping for VoucherCartResponse in VoucherMapper

--- src/main/java/com/example/bookverseserver/mapper/VoucherMapper.java
+++ src/main/java/com/example/bookverseserver/mapper/VoucherMapper.java
@@ -1,6 +1,7 @@
 package com.example.bookverseserver.mapper;
 
 import com.example.bookverseserver.dto.request.Voucher.VoucherRequest;
+import com.example.bookverseserver.dto.response.Voucher.VoucherCartResponse;
 import com.example.bookverseserver.dto.response.Voucher.VoucherResponse;
 import com.example.bookverseserver.entity.Order_Payment.Voucher;
 import org.mapstruct.Mapper;
@@ -11,5 +12,7 @@ public interface VoucherMapper {
     Voucher toVoucher(VoucherRequest request);
 
     VoucherResponse toVoucherResponse(Voucher voucher);
+
+    VoucherCartResponse toVoucherCartResponse(Voucher voucher);
 }
 
