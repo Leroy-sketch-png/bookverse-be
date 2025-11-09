08f464f6f7165631b35b8293c85757e7a655a3da
MKwang <tmq1512005@gmail.com>
2025-11-09 17:42:46 +0700

    feat: add CartItemMapper and repositories for CartItem and Cart

---
+++ src/main/java/com/example/bookverseserver/repository/CartItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.bookverseserver.repository;
+
+import com.example.bookverseserver.entity.Order_Payment.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+    Optional<CartItem> findByCartIdAndListingId(Long cartId, Long listingId);
+}
