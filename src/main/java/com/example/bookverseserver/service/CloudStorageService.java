c3897575039609306e322379b62b2e9787e789cd
Vo Trung Tin <tinvo2005@gmail.com>
2026-01-06 01:43:26 +0700

    feat: upload image onto Cloudinary

--- src/main/java/com/example/bookverseserver/service/CloudStorageService.java
+++ src/main/java/com/example/bookverseserver/service/CloudStorageService.java
@@ -1,17 +1,35 @@
 package com.example.bookverseserver.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Map;
+
 @Service
+@RequiredArgsConstructor
 public class CloudStorageService {
 
-    public String uploadFile(MultipartFile file) {
-        // Upload logic
-        return "uploaded_url";
-    }
+    private final Cloudinary cloudinary;
 
-    public void deleteFile(String fileUrl) {
-        // Delete logic
+    public String uploadFile(MultipartFile file) {
+        try {
+            // Upload file lên Cloudinary
+            // "folder": "bookverse_avatar" -> Nó sẽ tạo thư mục tên này trên Cloud cho gọn
+            Map uploadResult = cloudinary.uploader().upload(file.getBytes(),
+                    ObjectUtils.asMap(
+                            "folder", "bookverse_avatar",
+                            "resource_type", "auto"
+                    ));
+
+            // Trả về URL ảnh online (https://res.cloudinary.com/...)
+            return (String) uploadResult.get("secure_url");
+
+        } catch (IOException e) {
+            throw new RuntimeException("Upload ảnh thất bại: " + e.getMessage());
+        }
     }
 }
